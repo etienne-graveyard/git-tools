f5ff5f8044f90d59bffe49bd455dc54455740a91
Etienne Dldc <etienne.dldc@outlook.fr>
2016-09-18 00:13:13 +0200

    Add BASEDIR to able aliasing

--- gitarchive.sh
+++ gitarchive.sh
@@ -11,7 +11,6 @@
 #
 # Your remote repository is left untouched by this script.
 
-
 if [ "$1" = "" ]
 then
   echo -e "\e[33mUsage: bash $0 <git repositiry clone URL>\e[39m" 1>&2
@@ -19,6 +18,7 @@ then
 fi
 
 # Variable definitions
+BASEDIR=$(dirname "$0")
 GITURL=$1
 GITNAME=${GITURL##*/}
 GIT=${GITURL#*@}
@@ -29,34 +29,34 @@ USER=${USERREPO%/*}
 REPO=${USERREPO#*/}
 
 FOLDERNAME=${GITNAME%.git}
-TARNAME="$(date +%Y%m%d)/${PLATFORM}/${USER}/${REPO}.tgz"
+ARCHIVEPATH="$(date +%Y%m%d)/${PLATFORM}/${USER}/${REPO}.tgz"
 
 # create folder
-mkdir -p "$(date +%Y%m%d)/${PLATFORM}/${USER}"
+mkdir -p "${BASEDIR}/$(date +%Y%m%d)/${PLATFORM}/${USER}"
 
 # Set chmod
-sudo chmod -R 777 .
+sudo chmod -R 777 ${BASEDIR}
 
 # If file exist, exit
-if [ -e "./$TARNAME" ]
+if [ -e "${BASEDIR}/${ARCHIVEPATH}" ]
 then
-  echo -e "\e[94m➼ $TARNAME already exist.\e[39m"
+  echo -e "\e[94m➼ ${ARCHIVEPATH} already exist.\e[39m"
   exit 1
 else
-  echo -e "\e[95m➼ Start backup of \e[92m$GITNAME\e[39m"
+  echo -e "\e[95m➼ Start backup of \e[92m${GITNAME}\e[39m"
 fi
 
 # Clone the repos and go into the folder
-echo -e "\e[90m ➜ git clone --recursive $GITURL $FOLDERNAME\e[39m"
-git clone --recursive $GITURL $FOLDERNAME
+echo -e "\e[90m ➜ git clone --recursive ${GITURL} ${BASEDIR}/${FOLDERNAME}\e[39m"
+git clone --recursive ${GITURL} "${BASEDIR}/${FOLDERNAME}"
 
 # If folder not exist, exit
-if [ -e "./$FOLDERNAME" ]
+if [ -e "${BASEDIR}/${FOLDERNAME}" ]
 then
-  cd $FOLDERNAME
+  cd "${BASEDIR}/${FOLDERNAME}"
 else
   echo -e "\e[31m➼ ***************************************************\e[39m"
-  echo -e "\e[31m➼ $FOLDERNAME does not exist, clone probably failed !\e[39m"
+  echo -e "\e[31m➼ ${FOLDERNAME} does not exist, clone probably failed !\e[39m"
   echo -e "\e[31m➼ ***************************************************\e[39m"
   exit 1
 fi
@@ -73,11 +73,11 @@ git pull --all
 git gc # Cleanup unnecessary files and optimize the local repository
 
 # Create an archive of the directory
-cd ../
-tar cfz "$TARNAME" "$FOLDERNAME/"
+cd "${BASEDIR}"
+tar cfz "${BASEDIR}/${ARCHIVEPATH}" "${BASEDIR}/${FOLDERNAME}/"
 
 # Remove the git clone
-rm -rf "./$FOLDERNAME"
+rm -rf "${BASEDIR}/${FOLDERNAME}"
 
-echo -e "\e[95mYour archived git repository is named \e[92m$TARNAME\e[39m"
+echo -e "\e[95mYour archived git repository is named \e[92m$ARCHIVEPATH\e[39m"
 echo -e "\e[95m➼ Done!\e[39m"
--- gitarchiveall.sh
+++ gitarchiveall.sh
@@ -1,9 +1,11 @@
-file="./repos.sh"
+BASEDIR=$(dirname "$0")
+
+file="${BASEDIR}/repos.sh"
 while read -r line; do
   if [[ "$line" =~ ^#.* ]];
   then
     echo -e "\e[92m$line\e[39m"
   else
-    bash ./gitarchive.sh "$line"
+    bash ${BASEDIR}/gitarchive.sh "$line"
   fi
 done < $file
